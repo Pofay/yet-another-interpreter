e030ecea916ba353eff1e154efeb9627618fec26
Pofay <gianCarlo.gilos@gmail.com>
2023-07-21 08:17:44 +0000

    feat: Defined AstPrinter

---
+++ SharpLox/AstPrinter.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpLox
+{
+    public class AstPrinter : Expr.IExprVisitor<String>
+    {
+        public string VisitBinaryExpr(Expr.Binary expr)
+        {
+            return Parenthesize(expr.Opr.Lexeme, expr.Left, expr.Right);
+        }
+
+        public string VisitGroupingExpr(Expr.Grouping expr)
+        {
+            return Parenthesize("group", expr.Expression);
+        }
+
+        public string VisitLiteralExpr(Expr.Literal expr/*  */)
+        {
+            if (expr.Value == null) return "nil";
+            return expr.Value.ToString();
+        }
+
+        public string VisitUnaryExpr(Expr.Unary expr)
+        {
+            return Parenthesize(expr.Opr.Lexeme, expr.Right);
+        }
+
+        private string Parenthesize(string name, params Expr[] exprs)
+        {
+            var builder = new StringBuilder();
+            builder.Append("(").Append(name);
+            foreach (var expr in exprs)
+            {
+                builder.Append(" ");
+                builder.Append(expr.Accept(this));
+            }
+            builder.Append(")");
+            return builder.ToString();
+        }
+    }
+}
